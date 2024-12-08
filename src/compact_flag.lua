a4d4ef8775ba7c5d9800485a22b04278eef3742f
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-12-08 07:53:30 -0300

    compact flags

--- src/compact_flag.lua
+++ src/compact_flag.lua
@@ -6,21 +6,19 @@ luargv.get_compact_flags = function(flag_name)
     end
 
     local founds = {}
-    local founds_size = 1
+    local founds_size = 0
     local flag_size = luargv.get_str_size(flag_name)
 
     local args_size = luargv.get_total_args_size()
 
     for i = 1, args_size do
         local current = luargv.get_arg_by_index(i)
-        print(current)
-        if false then
-            local divided = luargv.substr_func(current, i, flag_size)
-            if divided == flag_name then
-                local current_size = luargv.get_str_size(current)
-                founds[founds_size] = luargv.substr_func(current, flag_size, current_size)
-                founds_size = founds_size + 1
-            end
+        local possible_start = luargv.substr_func(current, 0, flag_size)
+        if possible_start == flag_name then
+            local current_size = luargv.get_str_size(current)
+            founds_size = founds_size + 1
+
+            founds[founds_size] = luargv.substr_func(current, flag_size + 1, current_size)
         end
     end
     return founds, founds_size
